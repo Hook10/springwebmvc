3c47142e5a659a6d393807d3d09146d5821587fe
Asike <sapasask@gmail.com>
2022-05-08 23:16:42 +0600

    Spring validator added to Person entity.

--- src/main/java/org/hook/springcourse/controllers/PeopleController.java
+++ src/main/java/org/hook/springcourse/controllers/PeopleController.java
@@ -2,6 +2,7 @@ package org.hook.springcourse.controllers;
 
 import org.hook.springcourse.dao.PersonDAO;
 import org.hook.springcourse.models.Person;
+import org.hook.springcourse.util.PersonValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +15,12 @@ import javax.validation.Valid;
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -40,6 +43,8 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors())
             return "people/new";
 
@@ -56,6 +61,9 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
+
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors())
             return "people/edit";
 
